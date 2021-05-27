cdbda659db3dfda44392f00e0b7ee7ea37924102
ispteam <iproteam2021@gmail.com>
2021-05-27 23:13:31 +0800

    first initialization of the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Switch, Route} from "react-router-dom";
 
+
 const App = () => {
   document.title = "HOME";
   return (
@@ -8,12 +9,14 @@ const App = () => {
       <Route exact path="/"> {/** To display exact page with path / */}
         <div>Home</div>
       </Route>
-      <Route exact path="/:id"> {/** To display exact page with path that contains a dynamic value EX: /1, /12sxxas23 */}
+      <Route path="/:id"> {/** To display exact page with path that contains a dynamic value EX: /1, /12sxxas23 */}
         <div>Dynamic page</div>
       </Route>
-      <Route render={()=><h1>404 Not found</h1>} /> {/** To display not found page if the url not match any of the above URLS */}
+      <Route  render={()=><h1>404 Not found</h1>} /> {/** To display not found page if the url not match any of the above URLS */}
     </Switch>
   );
 };
 
 export default App;
+
+
