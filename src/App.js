af0aadec7b83d18c59f49411f82d088fab546ea5
i3z101 <abofahad.en@gmail.com>
2021-05-27 19:40:52 +0800

    first initialization of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route} from "react-router-dom";
 
-function App() {
+const App = () => {
+  document.title = "HOME";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/"> {/** To display exact page with path / */}
+        <div>Home</div>
+      </Route>
+      <Route exact path="/:id"> {/** To display exact page with path that contains a dynamic value EX: /1, /12sxxas23 */}
+        <div>Dynamic page</div>
+      </Route>
+      <Route render={()=><h1>404 Not found</h1>} /> {/** To display not found page if the url not match any of the above URLS */}
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/reducers.js
@@ -0,0 +1,10 @@
+const initState = {};
+
+const reducers = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
