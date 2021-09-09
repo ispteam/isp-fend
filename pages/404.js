df43e6774d6e930ac51b0eee294ce24b353d995e
ispteam <iproteam2021@gmail.com>
2021-09-09 03:22:04 +0300

    make it better

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 const NotFound = () => {
     return <div>
-        <Image src={"/imgs/404.jpg"} layout='responsive' width="80" height="80" />
+        <Image src={"/imgs/404.jpg"} layout='responsive' width="80" height="80" alt="404" />
     </div>
 }
 
